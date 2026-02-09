657e7c2b74e17c1fed66cd935ca66f765d3cd81a
Bhawesh Kahar <bhawesh.kahar@newvisionsoftpune.com>
2026-02-09 19:29:30 +0530

    Initial commit

---
+++ nvsoftuser/src/main/java/com/nv/soft/nvsoftuser/NvsoftuserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nv.soft.nvsoftuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NvsoftuserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NvsoftuserServiceApplication.class, args);
+	}
+
+}
---
+++ nvsoftuser/src/test/java/com/nv/soft/nvsoftuser/NvsoftuserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nv.soft.nvsoftuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NvsoftuserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
