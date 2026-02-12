8ce76ef12093fa924c440966f2a6f1351e3f4d9a
bhawesh.kahar <bhawesh.kahar@newvision-software.com>
2026-02-12 22:10:33 +0530

    CrossOrigin add for react application access

--- nvsoftuser/src/main/java/com/nv/soft/nvsoftuser/controller/AuthController.java
+++ nvsoftuser/src/main/java/com/nv/soft/nvsoftuser/controller/AuthController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/user")
 public class AuthController {
 
